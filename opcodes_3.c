0d918b4435577123852fe86e61d48efd9c92e5b3
cyrusDev1 <cyrushoungue1240@gmail.com>
2022-06-16 14:25:21 +0100

    task 9

--- opcodes_3.c
+++ opcodes_3.c
@@ -17,6 +17,13 @@ void mod(stack_t **stack, unsigned int line)
         fprintf(stderr, "L%d: can't mod, stack too short\n", line);
         exit(EXIT_FAILURE);
     }
+
+    if ((*stack)->n == 0)
+    {
+        fprintf(stderr, "L%d: division by zero\n", line);
+        exit(EXIT_FAILURE);
+    }
+
     result = (*stack)->next->n % (*stack)->n;
     pop(stack, line);
     (*stack)->n = result;
