209633ddfb2be493cd378dbf7e4e69e4d3e3d506
cyrusDev1 <cyrushoungue1240@gmail.com>
2022-06-16 12:57:29 +0100

    task 1

--- opcodes_1.c
+++ opcodes_1.c
@@ -55,7 +55,7 @@ void pint(stack_t **stack, unsigned int line)
 {
     if (*stack == NULL)
     {
-        fprintf(stderr, "L%d: can't pint, stack empty", line);
+        fprintf(stderr, "L%d: can't pint, stack empty\n", line);
         exit(EXIT_FAILURE);
     }
     printf("%d\n", (*stack)->n);
